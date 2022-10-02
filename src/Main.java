1ac613f7931413979f6de1d1638cdf8e57f96b2f
liza1a <liza36046@gmail.com>
2022-10-02 23:27:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ДЗ 1");
+
+        System.out.println("Задание 1");
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Поздравляю! Ты совершеннолетний");
+        }
+
+        if (age < 18) {
+            System.out.println("Наслаждайся молодостью, пока нет 18");
+        }
+        System.out.println("Hello world!");
+
+
+        System.out.println("Задание 2");
+        if (age >= 7) {
+            System.out.println("Поздравляю! Ты ходишь в школу");
+        }
+        if (age >= 18) {
+            System.out.println("Поздравляю! Ты уже закончил школу и может отправляться в университет");
+        }
+        if (age >= 24) {
+            System.out.println("Поздравляю! Ты уже окончил университет пора искать первую работу");
+        }
+
+        System.out.println("Задание 3");
+
+
+        System.out.println("ДЗ 2");
+
+        System.out.println("Задание 1.");
+        if (age >= 18) {
+            System.out.println("Поздравляю! Ты совершеннолетний");
+        } else {
+            System.out.println("Наслаждайся молодостью, пока нет 18");
+        }
+
+        System.out.println("Задание 2.");
+        if (age >= 24) {
+            System.out.println("Поздравляю! Ты ходишь в школу");
+        } else {
+            System.out.println("Поздравляю! Ты уже закончил школу и может отправляться в университет");
+        }
+        if (age >= 24) {
+            System.out.println("Поздравляю! Ты уже окончил университет пора искать первую работу");
+        }
+
+
+        System.out.println("Задание 3.");
+
+
+
+        System.out.println("ДЗ 3");
+        System.out.println("Задание 1..");
+        
+    }
+}
